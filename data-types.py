829121db239bf8f404a1a38ee38aa4f0131a8706
Your Name <mikaylabeelders@gmail.com>
2021-04-14 14:00:42 +0200

    first commit

---
+++ data-types.py
@@ -0,0 +1,31 @@
+#Task 2
+
+list = [5, 1, 2, 3, 4]
+print(max(list))
+
+list2 = [10, 2, 45, 3, 5, 7, 2, 10, 45, 8, 10]
+print(list2)
+
+
+#Task (PP)
+
+message="We are getting a Pizza."
+print(message)
+print(message[0])
+print(type(message))
+print(message[0:5])
+
+q="lifechoices"
+print(q[::-1])
+
+#TaskQuiz2
+
+list =[1, 2, 3, 4, 5]
+print(sum(list))
+
+my =[4, 1, 5, 3, 2]
+my.sort()
+print(my)
+
+my.sort(reverse=True)
+print(my)
